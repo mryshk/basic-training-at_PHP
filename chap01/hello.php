83843e5c07502702184aedb654c49579fa2c4e1f
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-12-01 21:55:45 +0900

    learn EOD

--- chap01/hello.php
+++ chap01/hello.php
@@ -29,6 +29,15 @@
   $title = 'サーバーサイド技術の学び舎';
   $data1 = "サポートサイト\n「{$title}」へ";
   print $data1;
+  ?><br />
+  <?php
+  $str = 'PHP (PHP:Hypertext Preprocessor)';
+  $msg = <<<"EOD"
+  {$str}は、サーバーサイドで動作する簡易なスクリプト言語です。
+  まずは、本書でじっくり基礎固めしましょう。<br/>
+  "Let's start everyone!!"
+  EOD;
+  print $msg;
   ?>
 
 </body>
