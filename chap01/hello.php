666ae5f637eaa04a896620bebffbdef601cf9ee9
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-12-01 18:15:07 +0900

    first commit

--- chap01/hello.php
+++ chap01/hello.php
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
+
 <body>
-<?php
-print "こんにちは、世界！<br/>";
-print "こんにちは、皆さん！";
-?>
+  <?= "こんにちは、世界！" ?><br />
+  <?= "こんにちは、皆さん！" ?><br />
+  <?php
+  print "Hello World";
+  ?><br>
+  <?php
+  $msg = "こんにちは！";
+  print $msg;
+  ?><br>
+  <?php
+  const TAX = 1.1;
+  $price = 1000;
+  $sum = $price * TAX;
+  print $sum;
+  ?>
 </body>
+
 </html>
