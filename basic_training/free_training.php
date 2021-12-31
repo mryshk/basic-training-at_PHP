c974e1c1ab940b685a9c30dfb29234c3f49b34ac
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-12-31 18:37:38 +0900

    Add free_training

--- basic_training/free_training.php
+++ basic_training/free_training.php
@@ -2,14 +2,14 @@
 <?php
 $dot = "。";
 $x = 3;
-if ($x === 1){
+if ($x === 1) {
   print "{$x}は1です{$dot}";
-}else{
+} else {
   print "{$x}は1ではありません{$dot}";
 }
 ?>
 <?php
-$data = [1,2,3,4,"山田","鈴木"];
+$data = [1, 2, 3, 4, "山田", "鈴木"];
 [4 => $s, 5 => $r] = $data;
 print "{$s}<br/>";
 print $data[4];
@@ -18,18 +18,24 @@ print $data[4];
 <!-- 条件演算子 -->
 <?php
 $message = "lll";
-print $message?:"空です";
+print $message ?: "空です";
 ?>
 <!-- foreach分 -->
 <?php
-$data = ["田中","鈴木","佐藤","山田","相田"];
-foreach ($data as $value){
+$data = ["田中", "鈴木", "佐藤", "山田", "相田"];
+foreach ($data as $value) {
   print "{$value}になります";
 }
 ?>
 <?php
-$data = ["田中","鈴木","佐藤","山田","相田"];
-foreach ($data as $key => $value){
+$data = ["田中", "鈴木", "佐藤", "山田", "相田"];
+foreach ($data as $key => $value) {
   print "{$key}は{$value}になります。";
+}
+?>
+<!-- for文 -->
+<?php
+for ($i = 0; $i < 4; $i++) {
+  print "{$i}になります。<br/>";
 }
 ?>
