84f1466fac34b5d98d3cf9bac8d4942995624597
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-01 22:38:12 +0900

    Add comment & modify

--- basic_training/basic_training3.php
+++ basic_training/basic_training3.php
@@ -11,11 +11,11 @@ if ($x === 10) {
 <!-- 実行されるのは最初に合致したプロックのみ -->
 <?php
 $x = 0;
-if ($x>20){
+if ($x > 20) {
   print '変数$xは20より大きいです。';
-}elseif ($x >10){
+} elseif ($x > 10) {
   print '変数$xは10より大きいです。';
-}else{
+} else {
   print '変数$xは10以下です。';
 }
 ?>
@@ -23,13 +23,13 @@ if ($x>20){
 <?php
 $x = 1;
 $y = 0;
-if ($x ===1){
-  if ($y === 1){
+if ($x === 1) {
+  if ($y === 1) {
     print '変数$x,$yは1です。';
-  }else{
+  } else {
     print '変数$xは１ですが、$yは1ではありません。';
   }
-}else {
+} else {
   print '変数$xは1ではありません。';
 }
 ?>
@@ -45,9 +45,9 @@ else
 <!-- bool型の条件分岐 -->
 <?php
 $flag = "";
-if ($flag){
+if ($flag) {
   print 'OK';
-}elseif(!$flag){
+} elseif (!$flag) {
   print "No";
 }
 ?>
@@ -55,7 +55,7 @@ if ($flag){
 <!-- 等価演算子の条件分岐の際に最適 -->
 <?php
 $rank = 'NO';
-switch ($rank){
+switch ($rank) {
   case 'OK':
     print 'OK';
     break;
@@ -65,7 +65,7 @@ switch ($rank){
   case "Yes":
     print "YEs";
     break;
-  default :
+  default:
     print "???";
     break;
 }
@@ -73,7 +73,7 @@ switch ($rank){
 <!-- switch文には必ずbreakがいる。自動で止まる性能を持っていないから。 -->
 <?php
 $rank = "NO";
-switch ($rank){
+switch ($rank) {
   case "NO":
     print "NO";
     break;
@@ -86,114 +86,115 @@ switch ($rank){
 <!-- caseを列挙する。orのような意味。 -->
 <?php
 $drink = "ビール";
-switch ($drink){
+switch ($drink) {
   case "日本酒":
   case "ビーす":
   case "ビール":
   case "お酒":
     print "日本酒です。";
     break;
-  default :
-   print "お酒？";
+  default:
+    print "お酒？";
 }
 ?>
 <!-- match式-->
 <!-- あるパターンの値を変数に入れ込むことができる。 -->
 <?php
 $rank = "OK";
-$result = match($rank){
-  'OK'=>'OKですね〜',
-  "NO"=> "NO",
-  "YES"=> "YES",
+$result = match ($rank) {
+  'OK' => 'OKですね〜',
+  "NO" => "NO",
+  "YES" => "YES",
   default => "???"
 };
 $tt = "。";
-print $result.$tt;
+print $result . $tt;
 ?>
 <!-- match複数 -->
 <?php
 $drink = "ワイン";
-$result = match($drink){
-  "ビール","日本酒","お酒" => "ビールです",
-  "ブランデー","ワイン" => "西洋酒です"
+$result = match ($drink) {
+  "ビール", "日本酒", "お酒" => "ビールです",
+  "ブランデー", "ワイン" => "西洋酒です"
 };
-print $result.$tt;
+print $result . $tt;
 ?>
 <!-- match関数2 -->
 <?php
 $onigiri = "かやく";
-$result = match($onigiri){
-  "しゃけ","こんぶ","おかか" => "OK",
-  "明太子","梅干し","かやく" => "NO"
+$result = match ($onigiri) {
+  "しゃけ", "こんぶ", "おかか" => "OK",
+  "明太子", "梅干し", "かやく" => "NO"
 };
-print $result.$tt;
+print $result . $tt;
 ?>
 <!-- while -->
 <?php
 $i = 1;
-while ($i < 4){
+while ($i < 4) {
   print "{$i}番目のループです。<br/>";
   $i++;
 }
 ?>
+<!-- doヴァージョンは前置であるため、必ず一回は処理される。 -->
 <?php
-$i =4;
+$i = 4;
 do {
   print "{$i}番目のループです。<br/>";
   $i++;
-}while ($i<4);
+} while ($i < 4);
 ?>
 <!-- for文 -->
 <?php
-for ($i=1; $i<4; $i++){
+for ($i = 1; $i < 4; $i++) {
   print "{$i}番目のループです。<br/>";
 }
 ?>
 <?php
-for($i=1,$j=1; $result = $i*$j,$i<8;$i++,$j++){
+for ($i = 1, $j = 1; $result = $i * $j, $i < 8; $i++, $j++) {
   print "{$i}×{$j}={$result}<br/>";
 }
 ?>
 <?php
-for ($i=2,$j=3;$result=$i+$j,$i<5;$i++){
+for ($i = 2, $j = 3; $result = $i + $j, $i < 5; $i++) {
   print "{$i}番目になりまして、あと{$j}を足して{$result}。<br/>";
 }
 ?>
 <!-- foreach文 -->
 <?php
-$data = ["田中","鈴木","佐藤","山田","相田"];
-foreach ($data as $value){
+$data = ["田中", "鈴木", "佐藤", "山田", "相田"];
+foreach ($data as $value) {
   print "{$value}になります。<br/>";
 }
 ?>
 <!-- foreach 連想配列Ver -->
 <?php
-$data =["田中"=>"24","山田"=>"97"," suzuki"=>"34"];
-foreach($data as $key => $value){
+$data = ["田中" => "24", "山田" => "97", " suzuki" => "34"];
+foreach ($data as $key => $value) {
   print "{$key}は年齢{$value}歳です。<br/>";
 }
 ?>
 <!-- 通常配列でもkey&valueは使用可能 -->
 <!-- ただしkeyがインデック番号になる -->
 <?php
-$data =["田中","鈴木","佐藤","山田","相田"];
-foreach ($data as $key => $value){
+$data = ["田中", "鈴木", "佐藤", "山田", "相田"];
+foreach ($data as $key => $value) {
   print "{$value}は年齢{$key}歳です。<br/>";
 }
 ?>
 <!-- foreachのforバージョン -->
 <?php
-$data = ["田中","鈴木","佐藤","山田","相田"];
-for ($i=0;$i<count($data);$i++){
+$data = ["田中", "鈴木", "佐藤", "山田", "相田"];
+for ($i = 0; $i < count($data); $i++) {
   print "{$data[$i]}になります。<br/>";
 }
 ?>
 <!-- foreachの参照渡し -->
 <?php
-$data =["田中","鈴木","佐藤","山田","相田"];
+$data = ["田中", "鈴木", "佐藤", "山田", "相田"];
 $value = [];
-foreach ($data as $i){
-  array_push($value,$i);
+foreach ($data as $i) {
+  array_push($value, $i);
 }
 print_r($data);
 print_r($value);
